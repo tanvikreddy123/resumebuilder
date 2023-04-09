805bdebb691fc74a7841e414d383c7800fcde71e
tanvikreddy123 <95277075+tanvikreddy123@users.noreply.github.com>
2023-04-09 22:41:44 +0530

    final commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,20 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "resumebuilder": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "resumebuilderf6e15656": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
 
 import "./App.css";
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports';
+
+import { withAuthenticator, Authenticator } from '@aws-amplify/ui-react';
+
+
+
+Amplify.configure(config);
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header className="App-header">
+        <Authenticator/>
+        <h2>My app content</h2>
+      </Header>
       <Body />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
